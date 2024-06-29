0026a319e812123ab85ad6e9c37d8b2d46fc6f99
Ashis Anshuman <165242514+Ashis-Anshuman@users.noreply.github.com>
2024-06-29 18:48:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+
+  console.log(Math.floor(window.innerWidth / 270));
+});
+
+//TOGGLE
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
+
+
+
+const searchButton = document.getElementById('searchButton');
+const searchInput = document.getElementById('searchInput');
+
+searchButton.addEventListener('click', () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    const movieItems = document.querySelectorAll('.movie-list-item');
+    movieItems.forEach(item => {
+        const title = item.querySelector('.movie-list-item-title').textContent.toLowerCase();
+        const description = item.querySelector('.movie-list-item-desc').textContent.toLowerCase();
+        
+        if (title.includes(searchTerm) || description.includes(searchTerm)) { 
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
+
+document.getElementsByClassName('fas fa-home').addEventListener('click+',()=>{
+  window.location.href = "index.html";
+})
+
+
